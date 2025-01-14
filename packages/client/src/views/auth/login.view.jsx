fceaf0e7b1841ab3c40585d6ac2b23e0e5489c40
lakshyaraj2006 <lakshyarajdash25@gmail.com>
2025-01-14 19:07:18 +0530

    Lesson 2 - Completed

--- packages/client/src/views/auth/login.view.jsx
+++ packages/client/src/views/auth/login.view.jsx
@@ -1,19 +1,34 @@
-import React from 'react'
+import React from 'react';
+import { useForm } from "react-hook-form"
 
 const Login = () => {
+    const {
+        handleSubmit,
+        register,
+        formState: { errors }
+    } = useForm({ mode: 'all' });
+
+    const onSubmit = (data) => {
+        console.log(data);
+    }
+
     return (
         <div className='h-screen bg-base-300 flex items-center justify-center'>
             <div className='p-4 bg-white rounded w-2/5'>
                 <h1 className='text-primary text-2xl text-center underline font-semibold'>Login Using Credentials</h1>
 
-                <form>
+                <form method='post' onSubmit={handleSubmit(onSubmit)}>
                     <div className='flex flex-col gap-2 my-3'>
                         <label htmlFor="username" className='label-text'>Username</label>
                         <input
                             type="text"
                             id="username"
                             placeholder="Enter your username"
-                            className="input input-bordered input-primary w-full" />
+                            className="input input-bordered input-primary w-full"
+                            {...register('username',
+                                { required: { value: true, message: 'Username is required!' } }
+                            )} />
+                        {errors?.username && <p className='text-error'>{errors?.username?.message}</p>}
                     </div>
                     <div className='flex flex-col gap-2 my-3'>
                         <label htmlFor="password" className='label-text'>Password</label>
@@ -21,7 +36,12 @@ const Login = () => {
                             type="password"
                             id="password"
                             placeholder="Enter your password"
-                            className="input input-bordered input-primary w-full" />
+                            className="input input-bordered input-primary w-full"
+                            {...register('password',
+                                { required: { value: true, message: 'Password is required!' } }
+                            )}
+                        />
+                        {errors?.password && <p className='text-error'>{errors?.password?.message}</p>}
                     </div>
                     <button className='btn btn-primary btn-block'>Login</button>
                 </form>
--- packages/client/src/views/auth/register.view.jsx
+++ packages/client/src/views/auth/register.view.jsx
@@ -1,19 +1,41 @@
 import React from 'react'
+import { useForm } from 'react-hook-form'
 
 const Register = () => {
+    const {
+        handleSubmit,
+        register,
+        watch,
+        formState: { errors }
+    } = useForm({ mode: 'all' });
+
+    const password = watch('password');
+
+    const onSubmit = (data) => {
+        console.log(data);
+    }
+
     return (
         <div className='h-screen bg-base-300 flex items-center justify-center'>
             <div className='p-4 bg-white rounded w-2/5'>
                 <h1 className='text-primary text-2xl text-center underline font-semibold'>Register For New Account</h1>
 
-                <form>
+                <form method='post' onSubmit={handleSubmit(onSubmit)}>
                     <div className='flex flex-col gap-2 my-3'>
                         <label htmlFor="username" className='label-text'>Username</label>
                         <input
                             type="text"
                             id="username"
                             placeholder="Choose unique username"
-                            className="input input-bordered input-primary w-full" />
+                            className="input input-bordered input-primary w-full"
+                            {...register('username',
+                                { 
+                                    required: { value: true, message: 'Username is required!' },
+                                    minLength: {value: 6, message: 'Username must contain atleast 6 characters!'},
+                                    maxLength: {value: 12, message: 'Username must contain atmost 12 characters!'}
+                                }
+                            )} />
+                        {errors?.username && <p className='text-error'>{errors?.username?.message}</p>}
                     </div>
                     <div className='flex flex-col gap-2 my-3'>
                         <label htmlFor="email" className='label-text'>Email</label>
@@ -21,7 +43,14 @@ const Register = () => {
                             type="email"
                             id="email"
                             placeholder="Enter email address"
-                            className="input input-bordered input-primary w-full" />
+                            className="input input-bordered input-primary w-full"
+                            {...register('email',
+                                {
+                                    required: { value: true, message: 'Email is required!' },
+                                    pattern: { value: /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/, message: 'Invalid email address!' }
+                                }
+                            )} />
+                        {errors?.email && <p className='text-error'>{errors?.email?.message}</p>}
                     </div>
                     <div className='flex flex-col gap-2 my-3'>
                         <label htmlFor="password" className='label-text'>Password</label>
@@ -29,7 +58,14 @@ const Register = () => {
                             type="password"
                             id="password"
                             placeholder="Choose a strong password"
-                            className="input input-bordered input-primary w-full" />
+                            className="input input-bordered input-primary w-full"
+                            {...register('password',
+                                { 
+                                    required: { value: true, message: 'Password is required!' },
+                                    minLength: {value: 8, message: 'Password must contain atleast 8 characters!'}
+                                }
+                            )} />
+                        {errors?.password && <p className='text-error'>{errors?.password?.message}</p>}
                     </div>
                     <div className='flex flex-col gap-2 my-3'>
                         <label htmlFor="cpassword" className='label-text'>Confirm Password</label>
@@ -37,7 +73,14 @@ const Register = () => {
                             type="password"
                             id="cpassword"
                             placeholder="Confirm your password"
-                            className="input input-bordered input-primary w-full" />
+                            className="input input-bordered input-primary w-full"
+                            {...register('cpassword',
+                                { 
+                                    required: { value: true, message: 'Confirm Password is required!' },
+                                    validate: (value) => password === value || 'Passwords do not match!'
+                                }
+                            )} />
+                        {errors?.cpassword && <p className='text-error'>{errors?.cpassword?.message}</p>}
                     </div>
                     <button className='btn btn-primary btn-block'>Register</button>
                 </form>
